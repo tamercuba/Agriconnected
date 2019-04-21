2a7435b7fd41d60103f4e2186e389c8c0bdb8fa0
tamercuba <tamercuba@gmail.com>
2019-04-21 14:14:48 -0300

    admin changes to all alien models

--- aliens/admin.py
+++ aliens/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models        import Alien, State
 
-# Register your models here.
+
+
+class AlienAdmin(admin.ModelAdmin):
+    list_display  = ['city', 'state', 'date']
+    search_fields = ['city']
+
+class StateAdmin(admin.ModelAdmin):
+    list_display  = ['initials', 'name', 'count']
+    search_fields = ['initials']
+
+admin.site.register(Alien, AlienAdmin)
+admin.site.register(State, StateAdmin)
