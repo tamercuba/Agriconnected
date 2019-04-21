1abe2c6548b43d925ad494537d45783f41dae0f8
tamercuba <tamercuba@gmail.com>
2019-04-21 14:14:25 -0300

    home view created

--- aliens/views.py
+++ aliens/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http      import HttpResponse
+from .forms           import AlienForm
 
-# Create your views here.
+def home(request):
+    template_name = 'home.html'
+    if request.method == 'POST':
+        form = AlienForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    form    = AlienForm()
+    context = {
+        'form': form
+    }
+    return render(request, template_name, context)
