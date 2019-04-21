c182cb48759e6846deab99e88218afa5c6c820f7
tamercuba <tamercuba@gmail.com>
2019-04-21 18:19:53 -0300

    Extras for the bootstrap date template

--- aliens/forms.py
+++ aliens/forms.py
@@ -4,13 +4,13 @@ from django.conf  import settings
 from .models      import Alien, State
 
 
-YEARS = [x for x in range(1901,2019)]
-class AlienForm(forms.ModelForm):
-    city  = forms.CharField(label='Cidade', max_length=50)
-    state = forms.ModelChoiceField(label='Estado',queryset=State.objects.all(), widget=forms.Select)
-    date  = forms.DateField(required=True, widget=forms.SelectDateWidget)
 
+class AlienForm(forms.ModelForm):
+    city  = forms.CharField(label='Cidade')
+    state = forms.ModelChoiceField(label='Estado',queryset=State.objects.all())
+    date  = forms.DateInput(attrs={'class':'datepicker'})
 
     class Meta:
         model  = Alien
+
         fields = '__all__'
