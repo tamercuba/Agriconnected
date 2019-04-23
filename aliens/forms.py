6e3aaadbba636767e1afb533a27b011bb60c851e
tamercuba <tamercuba@gmail.com>
2019-04-23 15:22:51 -0300

    Final forms version

--- aliens/forms.py
+++ aliens/forms.py
@@ -1,13 +1,14 @@
-from django       import forms
-#from django.forms import extras
-from django.conf  import settings
-from .models      import Alien, State
-
+from django                    import forms
+from django.conf               import settings
+from .models                   import Alien, State
 
 
 class AlienForm(forms.ModelForm):
-
+    city  = forms.CharField(label='Cidade', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    state = forms.ModelChoiceField(label='Estado',queryset=State.objects.all(),widget=forms.Select(attrs={'class': 'form-control'}))
     class Meta:
         model   = Alien
         fields  = ['city', 'state', 'date']
-        widgets = {'date': forms.DateInput(attrs={'class': 'datepicker'})} 
+        widgets = {
+            'date': forms.DateInput(attrs={'type': 'text', 'id': 'datepicker'})
+        }
