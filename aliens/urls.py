247a4504ff8a3053d87486f0ba4d80106a6c522d
tamercuba <tamercuba@gmail.com>
2019-04-23 15:23:34 -0300

    home and register urls added

--- aliens/urls.py
+++ aliens/urls.py
@@ -6,6 +6,7 @@ app_name = 'aliens'
 
 urlpatterns = [
     path('', views.home , name='home'),
+    path('register', views.register, name='register'),
     path('list/', views.aliens_list, name='alien_list'),
     path('counter/', views.alien_counter, name='alien_counter'),
 ]
