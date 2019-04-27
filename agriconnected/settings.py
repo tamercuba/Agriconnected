19cc9c683cd1c3275b752c1f9bfa363c80d42c2b
tamercuba <tamercuba@gmail.com>
2019-04-27 15:37:13 -0300

    allowed hosts misspelled

--- agriconnected/settings.py
+++ agriconnected/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = [localhost, herokuapp.com]
+ALLOWED_HOSTS = ['localhost', 'herokuapp.com']
 
 
 # Application definition
