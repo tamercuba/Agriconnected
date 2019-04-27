31f551ac83f2afcee72beb970bef5ca8f1feb3e3
tamercuba <tamercuba@gmail.com>
2019-04-27 15:49:09 -0300

    allowed hosts misspelled

--- agriconnected/settings.py
+++ agriconnected/settings.py
@@ -134,6 +134,6 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS=(
-    os.path.join(PROJECT_ROOT, 'static')
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
